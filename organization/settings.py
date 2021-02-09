343ac719bd08dfcbe72c1c7fa8c94958e026e953
Akbar <akbarmenglimuratov@gmail.com>
2021-02-09 12:40:07 +0500

    procfile, postgres, allowed_hosts

--- organization/settings.py
+++ organization/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ngipej^b8!qijsc9tx94+#p8s7#mf_u@ak41wi^tmyj&igsvmc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['organizationtexnopos.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -95,6 +95,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600,ssl_require=True)
+DATABASES['default'].update(db_from_env)
+
 
 
 # Password validation
